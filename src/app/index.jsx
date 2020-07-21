3402f881f1df73a388eb007cc71158b8952d40fb
talha-riaz <talha.riaz@mail.mcgill.ca>
2020-07-20 23:04:37 -0400

    adding project setup

---
+++ src/app/index.jsx
@@ -0,0 +1,9 @@
+import {store} from './store/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Dashboard} from './components/Dashboard';
+
+ReactDOM.render(
+    <Dashboard/>,
+    document.getElementById('app')
+);
